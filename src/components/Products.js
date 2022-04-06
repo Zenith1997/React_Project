db0d0778a1998cdda81b19bb590b0c42db10b2d9
Zenith Anthony <dabarerawza.19@uom.lk>
2022-04-06 09:30:21 +0530

    Grafana pie chart added

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,18 @@
-import Piechart from "../charts/PieChart";
+import "../charts/App.css"
 
 
 const Products =()=> {
   return (
     <div >
- 
       
-<Piechart/>
-    
+      
+
+    <div className="pie1">
+    <iframe title = "sd" src="http://localhost:3000/d/20Td4k8nk/pie1?orgId=1&from=1649195667879&to=1649217267879&theme=light" width="400" height="500" frameborder="0"></iframe>
+   
+    </div>
+
+
     </div>
   );
 }
