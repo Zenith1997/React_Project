872edffe37a79112cd94eac3dff53ece39a67bfa
Zenith Anthony <dabarerawza.19@uom.lk>
2022-04-06 10:41:36 +0530

    edited

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,14 +5,18 @@ const Products =()=> {
   return (
     <div >
       
-      
+    
 
     <div className="pie1">
-    <iframe title = "sd" src="http://localhost:3000/d/20Td4k8nk/pie1?orgId=1&from=1649195667879&to=1649217267879&theme=light" width="400" height="500" frameborder="0"></iframe>
+    <iframe title = "sd" src="http://localhost:3000/d/M06zvkU7k/pie-2?orgId=1&from=1649196361497&to=1649217961497&theme=light" width="400" height="500" frameborder="0"></iframe>
+    
+    <iframe title = "sd" src="http://localhost:3000/d/M06zvkU7k/pie-2?orgId=1&from=1649196361497&to=1649217961497&theme=light" width="400" height="500" frameborder="0"></iframe>
+    <iframe title = "sd" src="http://localhost:3000/d/M06zvkU7k/pie-2?orgId=1&from=1649196361497&to=1649217961497&theme=light" width="400" height="500" frameborder="0"></iframe>
    
     </div>
-
-
+  
+ 
+   
     </div>
   );
 }
